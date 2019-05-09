e5cfed73dba7abe9a4169bc13eee40d40e5b9004
JR <jfrazier@tiggee.com>
2019-05-09 17:28:30 -0400

    adding all the files

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser'
+
+const PORT = 5000;
+
+// Set up the express app
+const app = express();
+
+// Parse incoming requests data
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+// get all todos
+app.get('/api/v1/todos', (req, res) => {
+  res.status(200).send({
+    success: 'true',
+    message: 'todos retrieved successfully',
+    todos: "Here you go"
+  })
+});
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
+  });
