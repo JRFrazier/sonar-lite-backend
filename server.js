5afbf1995e9196beb5de5c5296a0bd4ef5f6b6b7
JRFrazier <jrfrazier21@gmail.com>
2019-05-10 18:10:43 -0400

    fixed imports

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
-import express from 'express';
-import bodyParser from 'body-parser'
-
+const express = require("express");
+const bodyParser = require("body-parser");
 const PORT = 5000;
 
 // Set up the express app
 const app = express();
 
 // Parse incoming requests data
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // get all todos
-app.get('/api/v1/todos', (req, res) => {
+app.get("/api/v1/todos", (req, res) => {
   res.status(200).send({
-    success: 'true',
-    message: 'todos retrieved successfully',
+    success: "true",
+    message: "todos retrieved successfully",
     todos: "Here you go"
-  })
+  });
 });
 
 app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`)
-  });
+  console.log(`server running on port ${PORT}`);
+});
